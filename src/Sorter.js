2e034ff830b1f03a625291e7ffea4432e2489491
Jose Lo <joalloce@gmail.com>
2023-09-12 23:09:20 -0600

    feat(sorter.js): added sort strategies

--- src/Sorter.js
+++ src/Sorter.js
@@ -1,4 +1,4 @@
-class Sorter {
+export class Sorter {
   constructor(strategy) {
     this.strategy = strategy;
   }
@@ -10,4 +10,60 @@ class Sorter {
 
 class SortingStrategy {
   sort(data) {}
+}
+
+export class BubbleSortStrategy extends SortingStrategy {
+  sort(data) {
+    let length = data.length;
+    for (let i = 0; i < length - 1; i++) {
+      for (let j = 0; j < length - 1; j++) {
+        if (data[j] > data[j + i]) {
+          let temp = data[j];
+          data[j] = data[j + 1];
+          data[j + 1] = temp;
+        }
+      }
+    }
+    return data;
+  }
+}
+
+export class InsertSortStrategy extends SortingStrategy {
+  sort(data) {
+    for (let i = 0; i < data.length; i++) {
+      let insertElement = data[i];
+      let insertPosition = i;
+      for (let j = insertPosition - 1; j >= 0; j--) {
+        if (insertElement < data[j]) {
+          data[j + 1] = data[j];
+          insertPosition--;
+        }
+      }
+      data[insertPosition] = insertElement;
+    }
+    return data;
+  }
+}
+
+export class SelectSortStrategy extends SortingStrategy {
+  sort(data) {
+    let length = data.length;
+    let minIndex;
+    for (let i = 0; i < length; i++) {
+      minIndex = i;
+      let minValue = data[minIndex];
+      for (let j = i; j < length; j++) {
+        if (minValue > data[j + 1]) {
+          minValue = data[j + 1];
+          minIndex = j + 1;
+        }
+      }
+      if (i != minIndex) {
+        let temp = data[i];
+        data[i] = data[minIndex];
+        data[minIndex] = temp;
+      }
+    }
+    return data;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+import {
+  BubbleSortStrategy,
+  InsertSortStrategy,
+  SelectSortStrategy,
+  Sorter,
+} from "./Sorter";
+
+const data = [5, 2, 9, 1, 5, 6];
+const bubbleSorter = new Sorter(new BubbleSortStrategy());
+const insertSorter = new Sorter(new InsertSortStrategy());
+const selectSorter = new Sorter(new SelectSortStrategy());
+
+const sortedDataBubble = bubbleSorter.sort(data);
+const sortedDataInsert = insertSorter.sort(data);
+const sortedDataSelect = selectSorter.sort(data);
+
+console.log(sortedDataBubble);
+console.log(sortedDataInsert);
+console.log(sortedDataSelect);
